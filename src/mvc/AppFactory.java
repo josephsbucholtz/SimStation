53f775c1fcb8d44cdcc19463a581875444106868
maikenpardon <166445025+maikenpardon@users.noreply.github.com>
2024-04-10 12:06:59 -0700

    Update AppFactory.java

--- src/mvc/AppFactory.java
+++ src/mvc/AppFactory.java
@@ -1,20 +1,13 @@
 package mvc;
+
 import java.util.*;
 
 public interface AppFactory {
-
     public Model makeModel();
-
-    public View makeView(Model m);
-
+    public View makeView(Model model);
     public String getTitle();
-
     public String[] getHelp();
-
     public String about();
-
     public String[] getEditCommands();
-
-    public Command makeEditCommand(Model model, String type, Object source);
-
+    public Command makeEditCommand(Model mode, String type, Object source);
 }
