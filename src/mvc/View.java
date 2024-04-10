896dad28a3df8d21805cec5633b9f50214f50cba
maikenpardon <166445025+maikenpardon@users.noreply.github.com>
2024-04-10 12:09:40 -0700

    Update View.java

--- src/mvc/View.java
+++ src/mvc/View.java
@@ -1,26 +1,36 @@
 package mvc;
 
+import mvc.Subscriber;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
 public class View extends JPanel implements Subscriber {
     protected Model model;
+
+
     public View(Model model) {
+        super();
         this.model = model;
         model.subscribe(this);
-        setSize(500, 500);
-        Border blackline = BorderFactory.createLineBorder(Color.black);
-        setBorder(blackline);
-        setBackground(Color.LIGHT_GRAY);
+        Border blackLine = BorderFactory.createLineBorder(Color.BLACK);
+        setBorder(blackLine);
+    }
+    public void setModel(Model newModel){
+        if(model !=null){
+            model.unsubscribe(this);
+        }
+        this.model = newModel;
+        if(newModel != null){
+            model.subscribe(this);
+            update();
+        }
+
+    }
+    @Override
+    public void update() {
+        this.repaint();
     }
 
-    public void update() { repaint();}
-
-    public void setModel(Model model) {
-        this.model.unsubscribe(this);
-        this.model = model;
-        model.subscribe(this);
-    }
 
 }
