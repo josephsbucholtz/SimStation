72274adf1d62381f9bd9727c050593fcc945b5a5
maikenpardon <166445025+maikenpardon@users.noreply.github.com>
2024-04-15 20:32:06 -0700

    Add files via upload

---
+++ src/FlockingFactory.java
@@ -0,0 +1,12 @@
+package flocking;
+import SimStation.*;
+import mvc.*;
+import java.awt.*;
+
+public class FlockingFactory extends SimulationFactory{
+    public FlockingFactory(Color background){
+        super(background);
+    }
+    public Model makeModel() { return new FlockingSimulation(); }
+    public String getTitle() { return "Flocking Simulator";}
+}
---
+++ src/FlockingSimulation.java
@@ -0,0 +1,26 @@
+package flocking;
+import mvc.*;
+import SimStation.*;
+import java.awt.*;
+
+public class FlockingSimulation extends Simulation{
+    public void populate(){
+        for(int i = 0; i< 50; i++){
+            addAgent(new Bird());
+        }
+    }
+    @Override
+    public String[] getStats(){
+        String[] stats = new String[this.size()];
+        for(int i =0; i<stats.length; i++){
+            Bird agent = (Bird)getAgents().get(i);
+            int j = i+1;
+            stats[i] = "#birds @ speed " + j + " = " + agent.getSpeed();
+        }
+        return stats;
+    }
+    public static void main(String[] args) {
+        AppPanel panel = new SimulationPanel(new FlockingFactory(Color.DARK_GRAY));
+        panel.display();
+    }
+}
