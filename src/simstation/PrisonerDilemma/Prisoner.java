2a0af92e84792243d73d4242a5a3086940907104
RonCodes88 <ronaldliyh@gmail.com>
2024-04-17 21:53:11 -0700

    fixed update method for prisonersDilemma

--- src/simstation/PrisonerDilemma/Prisoner.java
+++ src/simstation/PrisonerDilemma/Prisoner.java
@@ -43,30 +43,28 @@ public class Prisoner extends Agent {
     }
 
     public void update() {
+        heading = Heading.random();
         Prisoner pris2 = (Prisoner) world.getNeighbor(this, 10);
 
-        // Check if p2 is null
-        if (pris2 == null) {
-            return;
-        }
+        if (pris2 != null) {
+            boolean coop1 = this.cooperate();
+            boolean coop2 = pris2.cooperate();
 
-        boolean coop1 = this.cooperate();
-        boolean coop2 = pris2.cooperate();
-
-        pris2.partnerCheated = coop1;
-        this.partnerCheated = coop2;
-        if (coop1 && coop2) {
-            this.updateFitness(3);
-            pris2.updateFitness(3);
-        } else if (coop1 && !coop2) {
-            this.updateFitness(0);
-            pris2.updateFitness(5);
-        } else if (!coop1 && coop2) {
-            this.updateFitness(5);
-            pris2.updateFitness(0);
-        } else {
-            this.updateFitness(1);
-            pris2.updateFitness(1);
+            pris2.partnerCheated = coop1;
+            this.partnerCheated = coop2;
+            if (coop1 && coop2) {
+                this.updateFitness(3);
+                pris2.updateFitness(3);
+            } else if (coop1 && !coop2) {
+                this.updateFitness(0);
+                pris2.updateFitness(5);
+            } else if (!coop1 && coop2) {
+                this.updateFitness(5);
+                pris2.updateFitness(0);
+            } else {
+                this.updateFitness(1);
+                pris2.updateFitness(1);
+            }
         }
 
         // Move the agent according to its heading
